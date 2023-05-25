7fc4a3579e592a3089fabd5eddd00aa2316651e9
Chetamkcee <agbodikechetty@gmail.com>
2023-05-25 12:03:20 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ int main(void)
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
-	fprintf(STDERR, "yry");
+	fprintf(stderr, "yry");
 	return (0);
 }
