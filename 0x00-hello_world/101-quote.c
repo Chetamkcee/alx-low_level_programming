16949ccf3c6cc3a030272e00d92fbb9077b58b8f
Chetamkcee <agbodikechetty@gmail.com>
2023-05-26 03:27:46 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1)
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
