54b3a56668e882e42435d662a33a1ad73fdd2820
Chetamkcee <agbodikechetty@gmail.com>
2023-10-02 16:46:22 +0100

    7-print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,25 +3,15 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
-	char alphabet;
-	for (alphabet = 122; alphabet >= 97; alphabet--)
-		putchar(alphabet);
-	putchar('\n');
-	return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-	char alphabet;
-	for (alphabet = 122; alphabet >= 97; alphabet--)
-		putchar(alphabet);
-	putchar('\n');
-	return (0);
+	char alphabets;
+
+	for (alphabets = 122; alphabets >= 97; alphabets--)
+	{
+		putchar(alphabets);
+	}
+		putchar('\n');
+		return (0);
 }
