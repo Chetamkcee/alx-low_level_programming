2f2318a5a872bc193d6f20c0215e6233b9636490
Chetamkcee <agbodikechetty@gmail.com>
2023-11-24 01:01:24 +0100

    bit manipulation

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,47 +1,25 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * times_table - prints the 9 times table
  *
  * Return: Always 0 (Success)
  */
-void times_table(void)
+	int main(void)
 {
-int a;
-int b;
-int c;
-for (a = 0; a <= 9; a++)
-{
-for (b = 0; b <= 9; b++)
-{
-c = a * b;
-if ((c / 10) == 0)
-{
-if (b == 0)
-{
-_putchar ('0');
-}
-if (b != 0)
-{
-_putchar (' ');
-_putchar ((c % 10) + '0');
-}
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-else
-{
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-}
-_putchar ('\n');
-}
+	int a;
+	int b;
+	int result;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++);
+		{
+			result = a * b;
+			putchar((result / 10) + '0');
+			putchar((result % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+			putchar('\n');
 }
